f643e45bc5813dc0ab069492b4c6efd33c7ba2eb
yossiee <38056766+yossiee@users.noreply.github.com>
2020-04-25 23:17:04 +0900

    Introduce browserSync

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.browserSync({
+        proxy: '0.0.0.0:10080',
+        open: false
+    })
+    .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');

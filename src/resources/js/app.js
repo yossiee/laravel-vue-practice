858d38b24c536c31fbb40cea77f4a3d52d53d215
yossiee <38056766+yossiee@users.noreply.github.com>
2020-04-26 01:24:59 +0900

    Create routers

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,7 +1,10 @@
-require('./bootstrap');
 import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello world!</h1>'
+  router,
+  components: { App },
+  template: '<App />'
 })
---
+++ src/resources/js/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+// VueRouter インスタンス
+const router = new VueRouter({
+  routes
+})
+
+export default router

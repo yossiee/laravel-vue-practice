f3e9a790629e03a8257bd30769c5fc14808890fe
yossiee <38056766+yossiee@users.noreply.github.com>
2020-04-27 09:00:28 +0900

    Create Navbar/Footer components

---
+++ src/resources/js/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer">
+    <button class="button button--link">Logout</button>
+    <RouterLink class="button button--link" to="/logout">
+      Login / Register
+    </RouterLink>
+  </footer>
+</template>

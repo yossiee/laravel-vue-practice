1541a1d50335a631ad47fd2b87f0b188bea28aca
yossiee <38056766+yossiee@users.noreply.github.com>
2020-05-05 10:22:55 +0900

    :bomb: error handling

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -28,6 +28,10 @@ window.axios.interceptors.request.use(config => {
 
   return config
 })
+window.axios.interceptors.response.use(
+  response => response,
+  error => error.response || error
+)
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening

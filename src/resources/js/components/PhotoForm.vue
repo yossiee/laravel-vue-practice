5b078e44bb9f7f94e3e303a9884fbd0de7d00ad8
yossiee <38056766+yossiee@users.noreply.github.com>
2020-05-08 23:00:10 +0900

    :art: Create message component

--- src/resources/js/components/PhotoForm.vue
+++ src/resources/js/components/PhotoForm.vue
@@ -1,7 +1,10 @@
 <template>
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a photo</h2>
-    <form class="form" @submit.prevent="submit">
+    <div v-show="loading" class="panel">
+      <Loader>Sending your photo...</Loader>
+    </div>
+    <form v-show="! loading" class="form" @submit.prevent="submit">
       <div class="errors" v-if="errors">
         <ul v-if="errors.photo">
           <li v-for="msg in errors.photo" :key="msg">{{ msg }}</li>
@@ -20,8 +23,11 @@
 
 <script>
 import { CREATED, UNPROCESSABLE_ENTITY } from '../util'
-
+import Loader from './Loader.vue'
 export default {
+  components: {
+    Loader
+  },
   props: {
     value: {
       type: Boolean,
@@ -30,6 +36,7 @@ export default {
   },
   data () {
     return {
+      loading: false,
       preview: null,
       photo: null,
       errors: null
@@ -43,16 +50,13 @@ export default {
         this.reset()
         return false
       }
-
       // ファイルが画像ではなかったら処理中断
       if (! event.target.files[0].type.match('image.*')) {
         this.reset()
         return false
       }
-
       // FileReaderクラスのインスタンスを取得
       const reader = new FileReader()
-
       // ファイルを読み込み終わったタイミングで実行する処理
       reader.onload = e => {
         // previewに読み込み結果（データURL）を代入する
@@ -61,35 +65,38 @@ export default {
         // 結果として画像が表示される
         this.preview = e.target.result
       }
-
       // ファイルを読み込む
       // 読み込まれたファイルはデータURL形式で受け取れる（上記onload参照）
       reader.readAsDataURL(event.target.files[0])
       this.photo = event.target.files[0]
     },
+    // 入力欄の値とプレビュー表示をクリアするメソッド
     reset () {
       this.preview = ''
       this.photo = null
       this.$el.querySelector('input[type="file"]').value = null
     },
     async submit () {
+      this.loading = true
       const formData = new FormData()
       formData.append('photo', this.photo)
       const response = await axios.post('/api/photos', formData)
-
+      this.loading = false
       if (response.status === UNPROCESSABLE_ENTITY) {
         this.errors = response.data.errors
         return false
       }
-
       this.reset()
       this.$emit('input', false)
-
       if (response.status !== CREATED) {
         this.$store.commit('error/setCode', response.status)
         return false
       }
-
+      // メッセージ登録
+      this.$store.commit('message/setContent', {
+        content: '写真が投稿されました！',
+        timeout: 6000
+      })
       this.$router.push(`/photos/${response.data.id}`)
     }
   }
